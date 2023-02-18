359bf90b461f560fe78c2fe9d2a74dc1b3bc5916
aparecido-stos <120992776+aparecido-stos@users.noreply.github.com>
2023-02-18 20:01:54 -0300

    3,0

--- main.js
+++ main.js
@@ -1,3 +1,59 @@
+function verificar(){
+
+  var data = new Date()
+   var ano = data.getFullYear()
+   var fano = document.getElementById('txtano')
+  var res = document.getElementById('res')
+   if (fano.value.length == 0 || fano.value > ano){
+     alert('[Erro] verifique os dados e tente novamente!')
+   } else {
+     var fsex = document.getElementsByName('radsex')
+     var idade = ano - Number(fano.value)
+     var genero = ''
+     if (fsex[0].checked) {
+       genero = 'Homem'
+     } else if (fsex[1].checked) {
+       genero = 'Mulher'
+     }
+     res.innerHTML = `Detectamos: ${genero} com ${idade} anos.`
+   }
+ }
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //window.confirm("Gostou?")
 //var n1 = Number(window.prompt("Digite um numero?"))
 //var n2 = Number(window.prompt("Digite outro numero?"))
