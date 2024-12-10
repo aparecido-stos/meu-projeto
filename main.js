db10aeaa762721234812873a297f9bbbc67220e2
Aparecido Silva Dos Santos <aparecido.digio@gmail.com>
2024-12-09 21:01:19 -0300

    update hora e data

--- main.js
+++ main.js
@@ -1,33 +1,31 @@
 //funçao para linkar header.html
-document.querySelector('.header').innerHTML = fetch('header/header.html').then(response => response.text()).then(data => {
-document.querySelector('.header').innerHTML = data;
-});
-//fim da funçao para linkar header.html
-
+  document.querySelector('.header').innerHTML = fetch('/header/header.html').then(response => response.text()).then(data => {
+  document.querySelector('.header').innerHTML = data;
+  });
+  //fim da funçao para linkar header.html
 //funçao para linkar footer.html
-document.querySelector('.footer').innerHTML = fetch('footer/footer.html').then(response => response.text()).then(data => {
-document.querySelector('.footer').innerHTML = data;
-});
-//fim da funçao para linkar footer.html
-// escopo para chamar data e hora
-function exibirDataHora() {
-  const data = new Date();
-  const dia = data.getDate().toString().padStart(2, '0');
-  const mes = (data.getMonth() + 1).toString().padStart(2, '0');
-  const ano = data.getFullYear();
-  const hora = data.getHours().toString().padStart(2, '0');
-  const minutos = data.getMinutes().toString().padStart(2, '0');
-  const segundos = data.getSeconds().toString().padStart(2, '0');
+  document.querySelector('.footer').innerHTML = fetch('/footer/footer.html').then(response => response.text()).then(data => {
+  document.querySelector('.footer').innerHTML = data;
+  });
+  //fim da funçao para linkar footer.html
 
-  const dataHoraFormatada = `${dia}/${mes}/${ano} ${hora}:${minutos}:${segundos}`;
+  function exibirDataHora() {
+    const data = new Date();
+    const dia = data.getDate().toString().padStart(2, '0');
+    const mes = (data.getMonth() + 1).toString().padStart(2, '0');
+    const ano = data.getFullYear();
+    const hora = data.getHours().toString().padStart(2, '0');
+    const minutos = data.getMinutes().toString().padStart(2, '0');
+    const segundos = data.getSeconds().toString().padStart(2, '0');
   
-  document.getElementById("data-hora").textContent = dataHoraFormatada;
-}
-
-// Atualiza a data e hora a cada segundo
-setInterval(exibirDataHora, 1000);
-
-// Exibe imediatamente ao carregar a página
-exibirDataHora();
-// fim escopo para chamar data e hora
-
+    const dataHoraFormatada = `${dia}/${mes}/${ano} ${hora}:${minutos}:${segundos}`;
+    
+    document.getElementById("data-hora").textContent = dataHoraFormatada;
+  }
+  
+  // Atualiza a data e hora a cada segundo
+  setInterval(exibirDataHora, 1000);
+  
+  // Exibe imediatamente ao carregar a página
+  exibirDataHora();
+  // fim escopo para chamar data e hora
